1dca0bfa9638abee78ab8baf43e153e7222efc72
Henrik Sachse <t3x7m3@posteo.de>
2017-04-25 18:21:31 +0200

    Refactored run.sh
    Now the ```nginx.conf``` used by ```dockerize``` is used from the place which is expected by nginx at ```/etc/nginx/nginx.conf```. In case you want to use a custom configuration it must be mounted to ```/nginx.conf``` which is then copied to ```/etc/nginx/nginx.conf```.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-if test ! -e /nginx.conf; then
-    cp /etc/nginx/nginx.conf /nginx.conf
+if test -e /nginx.conf; then
+    cp -v /nginx.conf /etc/nginx/nginx.conf
 fi
-exec dockerize -stdout /var/log/nginx/access.log -stderr /var/log/nginx/error.log /usr/sbin/nginx -c /nginx.conf -g "daemon off;"
+exec dockerize -stdout /var/log/nginx/access.log -stderr /var/log/nginx/error.log /usr/sbin/nginx -c /etc/nginx/nginx.conf -g "daemon off;"
